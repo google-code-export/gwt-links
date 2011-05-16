145c3ffd4183cf00aff044b932e839389a148b3d
david.durham.jr@gmail.com <david.durham.jr@gmail.com@0e7bffe5-2f6f-aa0e-cdbe-84ab3aed1f29>
2011-05-16 14:51:53 +0000

    creating modular maven version

---
+++ branches/ddurham-maven/compile.bat
@@ -0,0 +1,3 @@
+cd E:\Projets\ModuloVox\workspaceDevModuloVox\gwt-links\
+call ant 
+copy .\target\gwt-links-1.2.jar ..\masterconf\war\WEB-INF\lib\
---
+++ branches/ddurham-maven/src/com/orange/links/client/DecorationShape.java
@@ -0,0 +1,10 @@
+package com.orange.links.client;
+
+import com.google.gwt.user.client.ui.Widget;
+
+public class DecorationShape extends AbstractShape{
+
+	public DecorationShape(DiagramController controller, Widget widget){
+		super(controller, widget);
+	}
+}
