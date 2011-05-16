6f3cad85225b31a7191aec5b789d354684e9bb0f
david.durham.jr@gmail.com <david.durham.jr@gmail.com@0e7bffe5-2f6f-aa0e-cdbe-84ab3aed1f29>
2011-05-16 16:16:17 +0000

    factor out to modules

--- branches/ddurham-maven/src/com/orange/links/client/event/ChangeOnDiagramHandler.java
+++
@@ -1,9 +0,0 @@
-package com.orange.links.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface ChangeOnDiagramHandler extends EventHandler{
-
-	public void onChangeOnDiagram(ChangeOnDiagramEvent event);
-	
-}
--- branches/ddurham-maven/src/com/orange/links/client/event/TieLinkHandler.java
+++
@@ -1,9 +0,0 @@
-package com.orange.links.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface TieLinkHandler extends EventHandler{
-
-	public void onTieLink(TieLinkEvent event);
-	
-}
