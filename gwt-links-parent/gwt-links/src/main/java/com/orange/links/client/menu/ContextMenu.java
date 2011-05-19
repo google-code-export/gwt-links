d387529dc6a55e2aab4ebde8cc289ce3151a43b7
david.durham.jr@gmail.com <david.durham.jr@gmail.com@0e7bffe5-2f6f-aa0e-cdbe-84ab3aed1f29>
2011-05-19 20:26:55 +0000

    factor out some init methods so subclasses can override expose ContextMenu menuBar for direct customization

--- gwt-links-parent/gwt-links/src/main/java/com/orange/links/client/menu/ContextMenu.java
+++ gwt-links-parent/gwt-links/src/main/java/com/orange/links/client/menu/ContextMenu.java
@@ -1,6 +1,7 @@
 package com.orange.links.client.menu;
 
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.PopupPanel;
@@ -17,9 +18,17 @@ public class ContextMenu extends PopupPanel {
         disableBrowserContextMenu(getElement());
     }
     
+    /** 
+     * Add a MenuItem with the GWT-Links style.
+     */
     public MenuItem addItem(MenuItem item) {
         item.addStyleName(LinksClientBundle.INSTANCE.css().connectionPopupItem());
         return menu.addItem(item);
+    }
+
+    public MenuItem addItem(String text, Command command) {
+        MenuItem item = new MenuItem(text, command);
+        return addItem(item);
     }
     
     /**
@@ -30,6 +39,12 @@ public class ContextMenu extends PopupPanel {
     public static native void disableBrowserContextMenu(Element e) /*-{
         e.oncontextmenu = function() { return false; };
     }-*/;
-    
+
+    /**
+     * Expose the underlying MenuBar for modification.
+     */
+    public MenuBar getMenu() {
+        return menu;
+    }
     
 }
