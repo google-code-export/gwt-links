00e024cc21c07babbec4309cd7ff4c00085bfd21
pierre.renaudin.fr@gmail.com <pierre.renaudin.fr@gmail.com@0e7bffe5-2f6f-aa0e-cdbe-84ab3aed1f29>
2011-05-20 08:13:55 +0000

    - New branch to improve performance

---
+++ branches/multi-canvas-solution/src/main/java/com/orange/links/client/utils/LinksClientBundle.java
@@ -0,0 +1,29 @@
+package com.orange.links.client.utils;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+public interface LinksClientBundle extends ClientBundle{
+
+	interface LinksCssResource extends CssResource {
+		
+		@ClassName("links-connection-popup")
+		public String connectionPopup();
+		
+		@ClassName("links-connection-popup-item")
+		public String connectionPopupItem();
+		
+		@ClassName("links-highlight-widget")
+		public String highlightWidget();
+		
+		@ClassName("links-translucide")
+		public String translucide();
+	}
+	
+	static final LinksClientBundle INSTANCE = GWT.create(LinksClientBundle.class);
+
+	@Source("GwtLinksStyle.css")
+	LinksCssResource css();
+
+}
